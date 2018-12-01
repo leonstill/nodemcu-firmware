3dd4327e6ba6ca04e5b3539ce3fa378b2100df09
Leon.L <leonstill@163.com>
2018-12-02 03:41:24 +0800

    update

--- build_bash_env.sh
+++ build_bash_env.sh
@@ -1,2 +1,29 @@
 #!/bin/sh
-sudo docker run --rm -ti -v `pwd`:/opt/nodemcu-firmware -v `pwd`/lua_leon:/opt/lua marcelstoer/nodemcu-build /bin/bash
+
+usage() 
+{
+    echo "Usage: ./build_bash_env.sh <help|build|client|lfs-image> [relative path]"
+    exit 1
+}
+
+# 编译firmware
+[ "$1" = "build" ] && {
+    sudo docker run --rm -ti -v `pwd`:/opt/nodemcu-firmware  \
+        marcelstoer/nodemcu-build build
+    exit $?
+}
+lua_dir=`pwd`/${2:-"lua_leon"}
+# 编译lsf image
+[ "$1" = "lfs-image" ] && {
+    sudo docker run --rm -ti -v `pwd`:/opt/nodemcu-firmware -v $lua_dir:/opt/lua \
+        marcelstoer/nodemcu-build lfs-image
+    exit $?
+}
+# 直接进入bashs
+[ "$1" = "client" ] && {
+    sudo docker run --rm -ti -v `pwd`:/opt/nodemcu-firmware -v $lua_dir:/opt/lua \
+        marcelstoer/nodemcu-build /bin/bash
+    exit $?
+}
+
+usage
