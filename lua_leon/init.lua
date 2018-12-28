722a208d1f00f81b18c1765e9fa05d5ff7bd0650
Leon.L <leonstill@163.com>
2018-12-29 00:23:06 +0800

    test

--- lua_leon/init.lua
+++ lua_leon/init.lua
@@ -1,20 +1,59 @@
+-- when somthing wrong in lua scripts and a dead loop happens, we could remove this file
+-- by sending the command 'file.remote("init.lua")' to NodeMCU while it is booting
+-- in the first second!
+-- by leon(leonstill@163.com) 2018.12.02
+
+-------------------------------------------------------------------------------
+-- GPIO init 
+-- 当使用node.dsleep()时，需要将管脚pin0和RST相连接，此时不要设置pin0（，或设置pin0为INPUT模式），否则会无限重启！
+--gpio.mode(0, gpio.INPUT)
+--gpio.mode(0, gpio.OUTPUT)
+--gpio.write(0, gpio.HIGH)
+
+-------------------------------------------------------------------------------
+-- init adc for vcc
+if adc.force_init_mode(adc.INIT_ADC)
+then
+  node.restart()
+  return -- don't bother continuing, the restart is scheduled
+end
+
+print("System voltage (mV):", adc.read(0))
+
+-------------------------------------------------------------------------------
+-- init timezone and sntp sync
+tz = require('tz')
+tz.setzone('Chongqing')
+-- https://nodemcu.readthedocs.io/en/latest/en/modules/sntp/#sntp.sync()
+-- 不需要明确调用rtctime.set()，因为当使能rtctime模块后，会自动调用。
+sntp.sync(nil, function(now)
+    local tm = rtctime.epoch2cal(now + tz.getoffset(now))
+    print(string.format("SNTP server sync time: %04d/%02d/%02d %02d:%02d:%02d", tm["year"], tm["mon"], tm["day"], tm["hour"], tm["min"], tm["sec"]))
+  end, function() 
+    print("SNTP server sync failed!")
+  end
+)
+
+
+-------------------------------------------------------------------------------
+-- init flash
 if node.flashindex() == nil then 
   node.flashreload('flash.img') 
 end
 -- LFS.blink ~= nil and LFS.blink or function(t, i) print("blink " .. t .. " with ".. i) end
 
-tmr.alarm(0, 1000, tmr.ALARM_SINGLE, 
+tmr.alarm(0, 1000, tmr.ALARM_SINGLE,
   function()
     local fi=node.flashindex
     local result = pcall(fi and fi'_init')
     print("LFS._init " .. (result and "ok" or "failed") )
     --local led_blink = node.flashindex('blink') or function() end
-    if LFS then 
-        if result then 
+    if LFS then
+        if result then
             local wifi = require("wifi_init");
-            wifi.start("task.lua", LFS.blink)
-        else 
-            LFS.blink(3, 150) 
+            wifi.start("task.lua")
+        else
+            LFS.blink(3, 150)
         end
     end
   end)
--- lua_leon/sendtemp.lua
+++ lua_leon/sendtemp.lua
@@ -1,6 +1,7 @@
 -- sendtemp.lua
 -- 检查温度并上报服务器
 --   by leonstill@163.com  2018-11-28
+local json = sjson
 
 m_dis={}
 function dispatch(m,t,pl)
@@ -17,14 +18,16 @@ end
 m_dis["/topic1"]=topic1func
 
 -- init mqtt client with logins, keepalive timer 120sec
-local m = mqtt.Client("nodemcu_temp01", 60, "liang", "peng")
+local m = mqtt.Client("nodemcu_temp02", 60, "liang", "peng")
 
 -- setup Last Will and Testament (optional)
 -- Broker will publish a message with qos = 0, retain = 0, data = "offline" 
 -- to topic "/lwt" if client don't send keepalive packet
 m:lwt("/lwt", "offline", 0, 0)
 
-m:on("connect", function(client) print("connected , heap size:"..node.heap()) end)
+m:on("connect", function(client) 
+    print("connected , heap size:"..node.heap()) 
+end)
 m:on("offline", function(client) print ("offline") end)
 
 -- on publish message receive event
@@ -32,7 +35,9 @@ m:on("message", dispatch)
 
 -- for TLS: m:connect("192.168.11.118", secure-port, 1)
 m:connect("114.215.120.146", 1883, 0, function(client)
-        print("connected")
+        print("client connected")
+        local data = { time=rtctime.get() }
+        client:publish("/login", json.encode(data), 1, 0, function(client) end)
         -- Calling subscribe/publish only makes sense once the connection
         -- was successfully established. You can do that either here in the
         -- 'connect' callback or you need to otherwise make sure the
@@ -43,35 +48,13 @@ m:connect("114.215.120.146", 1883, 0, function(client)
         --client:subscribe({["/topic"]=0, ["/blink"]=0}, function(client) print("subscribe success") end)
         -- publish a message with data = hello, QoS = 0, retain = 0
 
-        ------------------------------------------------------------------
-        -- get temperature
-        local get_temp = function() 
-            pin = 5
-            status, temp, humi, temp_dec, humi_dec = dht.read(pin)
-            if status == dht.OK then
-                -- Integer firmware using this example
-                -- print(string.format("DHT Temperature:%d.%03d;Humidity:%d.%03d\r\n",
-                --     math.floor(temp),
-                --     temp_dec,
-                --     math.floor(humi),
-                --     humi_dec
-                -- ))
-
-                -- Float firmware using this example
-                --print("DHT Temperature:"..temp..";".."Humidity:"..humi)
-                return { temp = temp, humi = humi }
-
-            elseif status == dht.ERROR_CHECKSUM then
-                print( "DHT Checksum error." )
-            elseif status == dht.ERROR_TIMEOUT then
-                print( "DHT timed out." )
-            end
-            return {}
-        end        
-
         tmr.alarm(1, 2000, tmr.ALARM_AUTO, function() 
-            local data = get_temp()
-            client:publish("/temp", sjson.encode(data), 0, 0, function(client) end)
+
+            local data = get_temp() or {}
+            if(data ~= nil) then
+                --pcall(LFS.blink, 1)
+                client:publish("/temp", json.encode(data), 0, 0, function(client) end)
+            end
         end)
 
     end, 
@@ -82,3 +65,28 @@ m:connect("114.215.120.146", 1883, 0, function(client)
 --m:close();
 -- you can call m:connect again
 
+------------------------------------------------------------------
+-- get temperature
+get_temp = function() 
+    pin = 5
+    status, temp, humi, temp_dec, humi_dec = dht.read(pin)
+    if status == dht.OK then
+        -- Integer firmware using this example
+        -- print(string.format("DHT Temperature:%d.%03d;Humidity:%d.%03d\r\n",
+        --     math.floor(temp),
+        --     temp_dec,
+        --     math.floor(humi),
+        --     humi_dec
+        -- ))
+
+        -- Float firmware using this example
+        --print("DHT Temperature:"..temp..";".."Humidity:"..humi)
+        return { temp = temp, humi = humi }
+
+    elseif status == dht.ERROR_CHECKSUM then
+        print( "DHT Checksum error." )
+    elseif status == dht.ERROR_TIMEOUT then
+        print( "DHT timed out." )
+    end
+    return nil
+end        
